f578f43ac68f5df0a412e601a6f0a3b0b76c4eb8
amarmahdin <120645744+amarmahdin@users.noreply.github.com>
2026-08-20 10:19:09 +0700

    join

--- application/models/Verifikasi_model.php
+++ application/models/Verifikasi_model.php
@@ -22,6 +22,7 @@ class Verifikasi_model extends CI_Model {
     public function get_all_viewer() {
        $this->db->select('tb_users.*, tb_dosen.nama as nama_dosen, tb_dosen.kode_dosen');
        $this->db->from('tb_users');
+       $this->db->join('tb_dosen', 'tb_dosen.id_dosen = tb_users.id_dosen', 'left');
     }
 
     public function approve($id_user, $id_dosen) {
